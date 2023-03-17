55daa3c84f97262bcd7ae4667661d809758d34ee
Your Name <you@example.com>
2023-03-18 02:30:07 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularPractice';
+  title = 'Students CEL';
+
+  id:any='';
+  name:string='';
+  department:string='';
+  semester:string='';
+  age:any='';
+
+  public studentInformation:any=[{id:this.id,name:this.name,department:this.department,semester:this.semester,age:this.age}];
+
+  
+  showInformation()
+  {
+    this.studentInformation.push({id:this.id,name:this.name,department:this.department,semester:this.semester,age:this.age});
+  }
+  
 }
